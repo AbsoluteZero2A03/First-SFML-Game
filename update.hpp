c917b4133ecb94e856becf3b0ff3df24a7a56535
ejs <absolutezero2a03@gmail.com>
2012-11-26 19:43:04 -0800

    Removed unnecessary comments in update.hpp

--- update.hpp
+++ update.hpp
@@ -11,7 +11,6 @@ namespace game
 			int update(sf::RenderWindow *, sf::Clock *); 
 			int draw(sf::RenderWindow *);
 			float Speed;
-//		protected:
 			sf::Sprite Sprite; 
 			
 	};
@@ -51,28 +50,4 @@ namespace game
 		
 		return 0;
 	}
-	/*void update(sf::RenderWindow * App, sf::Sprite * Sprite, sf::Clock * Clock, float * Speed)
-	{
-		float ElapsedTime = Clock.GetElapsedTime();
-		//float Speed = 50.f;
-		Clock.Reset();
-		
-		float x=Sprite.GetPosition().x;
-		float y=Sprite.GetPosition().y;
-		
-		if (App.GetInput().IsKeyDown(sf::Key::Left)) x -= Speed * ElapsedTime;
-		if (App.GetInput().IsKeyDown(sf::Key::Right)) x += Speed * ElapsedTime;
-		if (App.GetInput().IskeyDown(sf::Key::Up)) y -= Speed * ElapsedTime;
-		if (App.GetInput().IsKeyDown(sf::Key::Down)) y += Speed * ElapsedTime;
-		
-		Sprite.SetX(x);
-		Sprite.SetY(y);
-		
-	}
-	
-	void draw(sf::RenderWindow * App, sf::Sprite * Sprite)
-	{
-		App.Draw(Sprite)
-	}*/
-	
 }
