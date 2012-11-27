b0d12e08d9a922d9c5fddc28d1d53a37fba706dc
ejs <absolutezero2a03@gmail.com>
2012-11-26 19:36:34 -0800

    Removed unnecessary comments.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,6 @@ int main()
 {
 	sf::RenderWindow App;
 	App.Create(sf::VideoMode(500,400), "SFML Window");
-	/*
-	sf::Sprite Sprite;
-	Sprite.SetImage(Image);
-	
-	float Speed = 50.f;*/
 	
 	sf::Image Image;
 	if (!Image.LoadFromFile("Images/ball.png")) { return EXIT_FAILURE; }
@@ -25,24 +20,10 @@ int main()
 		sf::Event Event;
 		while (App.GetEvent(Event)) 
 		{
-			if (Event.Type == sf::Event::Closed) App.Close();
+			if (Event.Type == sf::Event::Closed) 
+				App.Close();
 		}
 		App.Clear();
-		/*float ElapsedTime = Clock.GetElapsedTime();
-		
-		Clock.Reset();
-		
-		float x=Sprite.GetPosition().x;
-		float y=Sprite.GetPosition().y;
-		
-		if (App.GetInput().IsKeyDown(sf::Key::Left)) x -= Speed * ElapsedTime;
-		if (App.GetInput().IsKeyDown(sf::Key::Right)) x += Speed * ElapsedTime;
-		if (App.GetInput().IsKeyDown(sf::Key::Up)) y -= Speed * ElapsedTime;
-		if (App.GetInput().IsKeyDown(sf::Key::Down)) y += Speed * ElapsedTime;
-		
-		Sprite.SetX(x);
-		Sprite.SetY(y);
-		App.Draw(Sprite);*/
 		
 		ball.update(&App, &Clock);
 		ball.draw(&App);
