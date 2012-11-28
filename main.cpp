f564ff7194bf6c50b1f1bcbc059888443931c181
ejs <absolutezero2a03@gmail.com>
2012-11-27 21:35:04 -0800

    Added (1) border(s). Ugh, this was unsuccessful...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include "update.hpp"
 
 int main() 
@@ -12,9 +13,13 @@ int main()
 	if (!Image.LoadFromFile("Images/ball.png")) { return EXIT_FAILURE; }
 	sf::Clock Clock;
 	
-	game::ball cball (&Image, 0.f, 0.f, 50.0);
+	game::ball cball (&Image, 3.f, 4.f, 50.0);
+	float border1[] = {0,0, 500,0};
+	float border2[] = {0,0, 0,400};
+	float border3[] = {500,0, 500,400};
+	float border4[] = {0,400, 500,400};
+	float * borders[] = {border1, border2, border3, border4};
 	
-
 	while (App.IsOpened()) 
 	{
 		sf::Event Event;
@@ -25,9 +30,9 @@ int main()
 		}
 		App.Clear();
 		
-		cball.update(&App, &Clock);
+		cball.update(&App, &Clock, &borders);
 		cball.draw(&App);
-
+		
 		App.Display();
 	}
 	
--- update.hpp
+++ update.hpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
-
+#include "basefs.hpp"
 namespace game 
 {
 	class ball 
 	{ 
 		public:
 			ball(sf::Image *,float,float,float);
-			int update(sf::RenderWindow *, sf::Clock *); 
-			int draw(sf::RenderWindow *);
+			void update(sf::RenderWindow *, sf::Clock *, float * (*)[4]); 
+			void draw(sf::RenderWindow *);
 			float Speed;
 			sf::Sprite Sprite; 
 			
@@ -21,12 +21,14 @@ namespace game
 		Sprite.SetY(y);
 		Speed = s;
 	}
-	int ball::update (sf::RenderWindow * App, sf::Clock * Clock)
+	void ball::update (sf::RenderWindow * App, sf::Clock * Clock, float * (*borders)[4])
 	{
 		float ElapsedTime = Clock->GetElapsedTime();
 		Clock->Reset();
 		float xx = Sprite.GetPosition().x;
 		float yy = Sprite.GetPosition().y;
+		float ww = Sprite.GetSize().x;
+		float hh = Sprite.GetSize().y;
 				
 		if (App->GetInput().IsKeyDown(sf::Key::Left)) xx -= Speed * ElapsedTime;
 		if (App->GetInput().IsKeyDown(sf::Key::Right)) xx += Speed * ElapsedTime;
@@ -34,17 +36,22 @@ namespace game
 		if (App->GetInput().IsKeyDown(sf::Key::Down)) yy += Speed * ElapsedTime;
 		
 		
+		if (CheckCollision(xx,yy,ww,hh,	*borders[0][0], *borders[0][1], *borders[0][2], *borders[0][3]) )
+			{
+				xx = Sprite.GetPosition().x;
+				yy = Sprite.GetPosition().y;
+			
+			}
+	
 		Sprite.SetX(xx);
 		Sprite.SetY(yy);
 		
-		return 0;
 		
 	}
 	
-	int ball::draw(sf::RenderWindow * App) 
+	void ball::draw(sf::RenderWindow * App) 
 	{
 		App->Draw(Sprite);
 		
-		return 0;
 	}
 }
